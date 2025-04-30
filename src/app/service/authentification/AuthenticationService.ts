52c740f9ffcc1248e96f42d6c97afffc9f817b57
Cyril <cyril.personne@outlook.fr>
2025-04-30 11:58:02 +0200

    first commit

---
+++ src/app/service/authentification/AuthenticationService.ts
@@ -0,0 +1,30 @@
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly TOKEN_KEY = 'auth_token'; // Clé pour le localStorage
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string) {
+    return this.http.post<any>('/api/auth/login', { username, password }).pipe(
+      tap((response) => {
+        if (response.token) {
+          this.saveToken(response.token); // Sauvegarde du token
+        }
+      })
+    );
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY); // Supprime le token
+  }
+}

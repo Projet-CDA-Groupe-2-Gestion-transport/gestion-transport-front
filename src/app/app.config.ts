05590f01c7afebd5df2a9919b3dd33054b08b6ac
EmanuelCottarel <emanuelcottarel@gmail.com>
2025-05-05 12:06:35 +0200

    1: Mise en place authentification, interceptor (jwt), composant home

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,18 @@ import {provideRouter} from '@angular/router';
 import {providePrimeNG} from 'primeng/config';
 import {routes} from './app.routes';
 import Basic from "./core/theme/app-theme";
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {authInterceptor} from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    providePrimeNG({ theme: Basic, ripple: false, inputStyle: 'outlined' }),
+    providePrimeNG({theme: Basic, ripple: false, inputStyle: 'outlined'}),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+    provideAnimationsAsync()
   ]
 };

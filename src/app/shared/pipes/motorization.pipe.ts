a49bd40a7b4c1f945848e615f0817c10c887397c
Cyril <cyril.personne@outlook.fr>
2025-07-08 20:04:27 +0200

    ajout pipe motorization et category

---
+++ src/app/shared/pipes/motorization.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {MotorizationEnum} from '../../core/enums/motorizationEnum';
+
+@Pipe({
+  name: 'motorization'
+})
+export class MotorizationPipe implements PipeTransform {
+
+  transform(motorization: MotorizationEnum): string {
+    switch (motorization) {
+      case MotorizationEnum.ELECTRIC:
+        return 'Électrique';
+      case MotorizationEnum.HYBRID:
+        return 'Hybride';
+      case MotorizationEnum.PETROL:
+        return 'Essence';
+      case MotorizationEnum.DIESEL:
+        return 'Diesel';
+      case MotorizationEnum.HYDROGEN:
+        return 'Hydrogène';
+      default:
+        return 'inconnu';
+    }
+  }
+}
